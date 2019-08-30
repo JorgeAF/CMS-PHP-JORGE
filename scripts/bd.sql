f2b8a1e7a6075557c5fc1e4a9f5f89af7da9dc5b
JorgeAF <teritabk@gmail.com>
2019-08-29 22:09:22 -0400

    backup de la bd

--- scripts/bd.sql
+++ scripts/bd.sql
@@ -1,32 +1,35 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Versión 4500
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 192.168.10.10 (MySQL 5.7.24-0ubuntu0.18.04.1)
-# Base de datos: cms
-# Tiempo de Generación: 2019-01-09 01:15:37 +0000
-# ************************************************************
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 30-08-2019 a las 02:07:26
+-- Versión del servidor: 5.7.19
+-- Versión de PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `bd`
+--
 
-# Volcado de tabla articulos
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `articulos`;
+--
+-- Estructura de tabla para la tabla `articulos`
+--
 
 CREATE TABLE `articulos` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_articulo` int(11) UNSIGNED NOT NULL,
   `titulo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `resumen` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `contenido` text COLLATE utf8mb4_unicode_ci,
@@ -36,95 +39,184 @@ CREATE TABLE `articulos` (
   `id_categoria` int(2) DEFAULT NULL,
   `tipo` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `slug` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `portada` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `portada` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `articulos`
+--
 
+INSERT INTO `articulos` (`id_articulo`, `titulo`, `resumen`, `contenido`, `publicado_por`, `fecha_creacion`, `hora_creacion`, `id_categoria`, `tipo`, `slug`, `portada`) VALUES
+(7, 'dsfsd', 'sdfsdf', 'dsfsdf', NULL, '2019-08-29', '07:25:03', 2, 'n', 'dsfsd', 'planilla.PNG'),
+(11, 'asdewfwewef', '', '', 13, '2019-08-29', '08:37:24', 3, 'p', 'asdewfwewef', '1.PNG'),
+(14, 'ahora sii', 'cdsfsdf', 'ddsdfsdfcsdcvererfefsfsdf', 13, '2019-08-29', '08:56:25', 1, 'n', 'ahora-sii', 'DSC_0001.JPG');
 
-# Volcado de tabla categorias
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `categorias`;
+--
+-- Estructura de tabla para la tabla `categorias`
+--
 
 CREATE TABLE `categorias` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `categoria` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id_categoria` int(11) UNSIGNED NOT NULL,
+  `categoria` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-LOCK TABLES `categorias` WRITE;
-/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `categorias`
+--
 
-INSERT INTO `categorias` (`id`, `categoria`)
-VALUES
-	(1,'Backend'),
-	(2,'FrontEnd'),
-	(3,'Inteligencia Artificial'),
-	(4,'Movil');
+INSERT INTO `categorias` (`id_categoria`, `categoria`) VALUES
+(1, 'Backend'),
+(2, 'FrontEnd'),
+(3, 'Inteligencia Artificial'),
+(4, 'Movil');
 
-/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
-
-# Volcado de tabla comentarios
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `comentarios`;
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
 
 CREATE TABLE `comentarios` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_comentario` int(11) UNSIGNED NOT NULL,
   `comentario` text COLLATE utf8mb4_unicode_ci,
   `id_usuario` int(2) DEFAULT NULL,
-  `id_articulo` int(3) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id_articulo` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `comentarios`
+--
 
+INSERT INTO `comentarios` (`id_comentario`, `comentario`, `id_usuario`, `id_articulo`) VALUES
+(1, 'holaaa es mi primerr comentario', 1, 1),
+(2, 'hola, mi nuevo comentario', 4, 4),
+(3, 'hola, mi nuevo comentario', 8, 3),
+(4, 'hola, mi nuevo comentario', 10, 2),
+(5, 'hola, mi nuevo comentario', 12, 3),
+(6, 'hola, mi nuevo comentario', 11, 2),
+(7, 'a mi me gusta php csm', 14, 1),
+(8, 'ahora si perro\r\n', 14, 1);
 
-# Volcado de tabla roles
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `roles`;
+--
+-- Estructura de tabla para la tabla `roles`
+--
 
 CREATE TABLE `roles` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `rol` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id_rol` int(11) UNSIGNED NOT NULL,
+  `rol` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `roles`
+--
 
-INSERT INTO `roles` (`id`, `rol`)
-VALUES
-	(1,'admin'),
-	(2,'usuario');
+INSERT INTO `roles` (`id_rol`, `rol`) VALUES
+(1, 'admin'),
+(2, 'usuario');
 
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
-
-# Volcado de tabla usuarios
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `usuarios`;
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
 
 CREATE TABLE `usuarios` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `apodo` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `password` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `apodo` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `id_rol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `usuarios`
+--
 
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apodo`, `password`, `email`, `id_rol`) VALUES
+(1, 'jorge', 'tbk', '202cb962ac59075b964b07152d234b70', 'jor@gmail.com', 1),
+(4, 'sdsa', 'dsad', '7815696ecbf1c96e6894b779456d330e', 'asd', 2),
+(8, 'juanito', 'mede', '6d0007e52f7afb7d5a0650b0ffb8a4d1', 'sdfsdf', 2),
+(10, 'lau', 'mede', '6d0007e52f7afb7d5a0650b0ffb8a4d1', 'dsfs', 2),
+(11, 'como', 'mede', '6d0007e52f7afb7d5a0650b0ffb8a4d1', 'ddsf\r\n                                            ', 2),
+(12, 'rata', 'mede', '6d0007e52f7afb7d5a0650b0ffb8a4d1', 'dsv', 2),
+(13, 'jawi', 'jawara', '48dc8d29308eb256edc76f25def07251', 'jawar@gmail.com', 1),
+(14, 'jawi', 'jawar', '48dc8d29308eb256edc76f25def07251', 'jawaris@gmail.com', 2);
 
+--
+-- Índices para tablas volcadas
+--
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+--
+-- Indices de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD PRIMARY KEY (`id_articulo`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  MODIFY `id_articulo` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
